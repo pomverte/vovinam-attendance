09bc2da70f28c9db03955b275a6b1210b88eeaad
pomverte <vietnem@yahoo.fr>
2017-12-10 00:23:50 +0100

    feat: liste de personnes récupérée en REST
    filtre sur la liste des personnes à afficher

--- src/App.vue
+++ src/App.vue
@@ -2,16 +2,52 @@
   <div id="app" class="container">
     <b-jumbotron header="Vovinam Viet Vo Dao" lead="Liste de présence">
     </b-jumbotron>
+    <b-container>
+      <b-row class="text-center">
+        <b-col sm="8">
+          <b-form-input v-model="search" type="text" placeholder="Nom du vo sinh"></b-form-input>
+          </br>
+          <b-table striped hover :items="userFilteredList" :per-page="10"></b-table>
+        </b-col>
+        <b-col sm="4">
+          <p>I'm here</p>
+        </b-col>
+      </b-row>
+    </b-container>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
+
 export default {
   name: 'app',
   data () {
     return {
-
+      search: '',
+      currentPage: 1,
+      userList: []
     }
+  },
+  computed: {
+    userFilteredList() {
+  	  var self = this;
+      return self.userList.filter(function(user) {
+        // TODO sanitize carateres speciaux
+        return (user.firstName + ' ' + user.lastName)
+          .toLowerCase().indexOf(self.search.toLowerCase()) >= 0;
+      });
+    }
+  },
+  created() {
+    var self = this;
+    axios.get('https://raw.githubusercontent.com/pomverte/vovinam-attendance/develop/users.json')
+      .then(function (response) {
+        self.userList = response.data;
+      })
+      .catch(function (error) {
+        console.log(error);
+      });
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 import BootstrapVue from "bootstrap-vue"
+import axios from 'axios'
 import App from './App.vue'
 import "bootstrap/dist/css/bootstrap.min.css"
 import "bootstrap-vue/dist/bootstrap-vue.css"
 
-Vue.use(BootstrapVue)
+Vue.use(BootstrapVue, axios)
 
 new Vue({
   el: '#app',
